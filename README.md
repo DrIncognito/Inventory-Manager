f20c004b17472a11c219b8b52ef5e110b1569f45
DrIncognito <152453201+DrIncognito@users.noreply.github.com>
2023-12-03 05:52:16 +0000

    Added a help command to the inventory.py script and updated the README.md file

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ This class encapsulates all the operations of the inventory management system.
 - `update_item(item_name, quantity, value)`: Updates the details of an item in the inventory.
 - `view_inventory()`: Returns all items in the inventory.
 - `audit_inventory()`: Audits all items in the inventory. Asks the user to confirm the quantity and value of each item. If the user does not confirm, it allows the user to edit the item details.
-
+- `help()`: Built in help message on how to use the inventory management library.
 ## Usage
 
 First, create an instance of the `Inventory` class:
@@ -35,4 +35,5 @@ inventory.remove_item("Apple")
 inventory.update_item("Apple", 200, 0.75)
 items = inventory.view_inventory()
 inventory.audit_inventory()
+help_msg = inventory.help()
 ```
--- inventory.py
+++ inventory.py
@@ -1,17 +1,60 @@
 import sqlite3
 
 class Inventory:
+    """
+    This class represents an inventory management system.
+    It uses SQLite as the database to store inventory data.
+    """
+
     def __init__(self):
+        """
+        Constructor method. Establishes a connection to the SQLite database and creates a cursor.
+        Also creates the inventory table if it does not exist.
+        """
         self.db = sqlite3.connect('inventory_db.sqlite')
         self.cursor = self.db.cursor()
         self.cursor.execute('''
             CREATE TABLE IF NOT EXISTS inventory(
-                item_name TEXT PRIMARY KEY,
-                quantity INTEGER,
-                value REAL)
+                item_name TEXT PRIMARY KEY,  # item_name is the primary key
+                quantity INTEGER,  # quantity is the number of items in stock
+                value REAL)  # value is the price of the item
         ''')
 
+    def help(self):
+        """
+        Prints a help message with a description of the class and its methods.
+        """
+        message = """
+        Inventory Management System
+
+        This class represents an inventory management system.
+        It uses SQLite as the database to store inventory data.
+
+        Methods:
+        - add_item(item_name, quantity, value): Adds an item to the inventory.
+        - remove_item(item_name): Removes an item from the inventory.
+        - update_item(item_name, quantity, value): Updates the details of an item in the inventory.
+        - view_inventory(): Returns all items in the inventory.
+        - audit_inventory(): Audits all items in the inventory.
+
+        Usage:
+        First, create an instance of the Inventory class:
+        inventory = Inventory()
+
+        Then, you can call the methods of the Inventory class to manage the inventory:
+        inventory.add_item("Apple", 100, 0.5)
+        inventory.remove_item("Apple")
+        inventory.update_item("Apple", 200, 0.75)
+        items = inventory.view_inventory()
+        inventory.audit_inventory()
+        """
+        print(message)
+
     def add_item(self, item_name, quantity, value):
+        """
+        Adds an item to the inventory.
+        Raises a ValueError if the item already exists in the inventory.
+        """
         try:
             query = "INSERT INTO inventory (item_name, quantity, value) VALUES (?, ?, ?)"
             values = (item_name, quantity, value)
@@ -23,6 +66,9 @@ class Inventory:
             print(f"An error occurred: {e}")
 
     def remove_item(self, item_name):
+        """
+        Removes an item from the inventory.
+        """
         try:
             query = "DELETE FROM inventory WHERE item_name = ?"
             values = (item_name,)
@@ -32,6 +78,9 @@ class Inventory:
             print(f"An error occurred: {e}")
 
     def update_item(self, item_name, quantity, value):
+        """
+        Updates the details of an item in the inventory.
+        """
         try:
             query = "UPDATE inventory SET quantity = ?, value = ? WHERE item_name = ?"
             values = (quantity, value, item_name)
@@ -41,11 +90,19 @@ class Inventory:
             print(f"An error occurred: {e}")
 
     def view_inventory(self):
+        """
+        Returns all items in the inventory.
+        """
         self.cursor.execute("SELECT * FROM inventory")
         rows = self.cursor.fetchall()
         return rows
 
     def audit_inventory(self):
+        """
+        Audits all items in the inventory.
+        Asks the user to confirm the quantity and value of each item.
+        If the user does not confirm, it allows the user to edit the item details.
+        """
         items = self.view_inventory()
         for item in items:
             item_name, quantity, value = item
@@ -55,3 +112,5 @@ class Inventory:
                 new_quantity = int(input("Enter the new quantity: "))
                 new_value = float(input("Enter the new value: "))
                 self.update_item(item_name, new_quantity, new_value)
+            else:
+                print("The quantity and value are confirmed.")
