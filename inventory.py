6d7d085cce6bfe5db3269edeebe84982581675ba
DrIncognito <152453201+DrIncognito@users.noreply.github.com>
2023-12-03 05:00:48 +0000

    Added audit function to inventory.py and modeled it into a class for more efficient use.

--- inventory.py
+++ inventory.py
@@ -1,94 +1,57 @@
 import sqlite3
-import pandas as pd
 
-# Establish a connection to the SQLite database
-# If the database does not exist, it will be created
-# This is done using the connect() function from the sqlite3 module
-db = sqlite3.connect('inventory_db.sqlite')
+class Inventory:
+    def __init__(self):
+        self.db = sqlite3.connect('inventory_db.sqlite')
+        self.cursor = self.db.cursor()
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS inventory(
+                item_name TEXT PRIMARY KEY,
+                quantity INTEGER,
+                value REAL)
+        ''')
 
-# Create a cursor object
-# The cursor is used to traverse the records from the result set
-cursor = db.cursor()
+    def add_item(self, item_name, quantity, value):
+        try:
+            query = "INSERT INTO inventory (item_name, quantity, value) VALUES (?, ?, ?)"
+            values = (item_name, quantity, value)
+            self.cursor.execute(query, values)
+            self.db.commit()
+        except sqlite3.IntegrityError:
+            raise ValueError("Item already exists in the inventory. Use the update option instead.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
-# Create the inventory table if it does not exist
-# The execute() function is used to perform SQL commands
-# The table has three columns: item_name, quantity, and value
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS inventory(
-        item_name TEXT PRIMARY KEY,  # item_name is the primary key
-        quantity INTEGER,  # quantity is the number of items in stock
-        value REAL)  # value is the price of the item
-''')
+    def remove_item(self, item_name):
+        try:
+            query = "DELETE FROM inventory WHERE item_name = ?"
+            values = (item_name,)
+            self.cursor.execute(query, values)
+            self.db.commit()
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
-# Function to add an item to the inventory
-def add_item(item_name, quantity, value):
-    """
-    This function adds an item to the inventory.
-    It takes three parameters: item_name, quantity, and value.
-    It raises a ValueError if the item already exists in the inventory.
-    """
-    try:
-        # Prepare the query and values
-        query = "INSERT INTO inventory (item_name, quantity, value) VALUES (?, ?, ?)"
-        values = (item_name, quantity, value)
-        
-        # Execute the query
-        cursor.execute(query, values)
-        
-        # Commit the transaction
-        db.commit()
-    except sqlite3.IntegrityError:
-        raise ValueError("Item already exists in the inventory. Use the update option instead.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    def update_item(self, item_name, quantity, value):
+        try:
+            query = "UPDATE inventory SET quantity = ?, value = ? WHERE item_name = ?"
+            values = (quantity, value, item_name)
+            self.cursor.execute(query, values)
+            self.db.commit()
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
-# Function to remove an item from the inventory
-def remove_item(item_name):
-    """
-    This function removes an item from the inventory.
-    It takes one parameter: item_name.
-    """
-    try:
-        # Prepare the query and values
-        query = "DELETE FROM inventory WHERE item_name = ?"
-        values = (item_name,)
-        
-        # Execute the query
-        cursor.execute(query, values)
-        
-        # Commit the transaction
-        db.commit()
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    def view_inventory(self):
+        self.cursor.execute("SELECT * FROM inventory")
+        rows = self.cursor.fetchall()
+        return rows
 
-# Function to update item details
-def update_item(item_name, quantity, value):
-    """
-    This function updates the details of an item in the inventory.
-    It takes three parameters: item_name, quantity, and value.
-    """
-    try:
-        # Prepare the query and values
-        query = "UPDATE inventory SET quantity = ?, value = ? WHERE item_name = ?"
-        values = (quantity, value, item_name)
-        
-        # Execute the query
-        cursor.execute(query, values)
-        
-        # Commit the transaction
-        db.commit()
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-# Function to view the inventory
-def view_inventory():
-    """
-    This function returns all items in the inventory.
-    """
-    # Execute the query
-    cursor.execute("SELECT * FROM inventory")
-    
-    # Fetch all the rows
-    rows = cursor.fetchall()
-    
-    return rows
+    def audit_inventory(self):
+        items = self.view_inventory()
+        for item in items:
+            item_name, quantity, value = item
+            print(f"\nItem: {item_name}\nQuantity: {quantity}\nValue: {value}")
+            confirm = input("Are the quantity and value correct? (yes/no): ")
+            if confirm.lower() != "yes":
+                new_quantity = int(input("Enter the new quantity: "))
+                new_value = float(input("Enter the new value: "))
+                self.update_item(item_name, new_quantity, new_value)
